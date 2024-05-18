0055540791880c9f01f0688e8a9a5ed48fab08ab
luismiaresse <luismiaresse03@gmail.com>
2024-05-18 15:20:06 +0200

    Add DB models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from knox.models import User
 
-# Create your models here.
+SCHEMA = "rest"
+
+# Album model
+class Album(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    creationdate = models.DateTimeField(auto_now=True)
+    lastupdate = models.DateTimeField(auto_now=True)
+    user = models.ManyToManyField(User)       # Creates intermediate table
+    
+    class Meta:
+        db_table = f'"{SCHEMA}"."album"'
+
+    def __str__(self):
+        return f"Album: {self.name}"
+
+# Media (images or videos) model
+class Media(models.Model):
+    id = models.AutoField(primary_key=True)
+    file = models.BinaryField()
+    creationdate = models.DateTimeField(auto_now=True)
+    kind = models.CharField(max_length=20)    # Can be 'image', 'video' or 'profile' (for profile pictures)
+    album = models.ManyToManyField(Album)     # Creates intermediate table 
+    
+    # Optional fields
+    location = models.CharField(max_length=50, null=True)
+    label = models.CharField(max_length=50, null=True)
+    
+    class Meta:
+        db_table = f'"{SCHEMA}"."media"'
+
+    def __str__(self):
+        return f"Media: {self.label}"
+    
+
