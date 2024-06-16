2c26f92bbf8cd24c4a024c02d75654576cae713c
luismiaresse <luismiaresse03@gmail.com>
2024-06-17 00:52:32 +0200

    Update image encoding/decoding

--- core/utils.py
+++ core/utils.py
@@ -1,11 +1,13 @@
 import base64
 
 def encode_image(file):
-    return base64.b64encode(file)
+    return base64.b64encode(file).decode('utf-8')
 
 def decode_image(img_from_db):
     if img_from_db is None:
         return None
-    if not isinstance(img_from_db, bytes):
-        return img_from_db
-    return img_from_db.decode('utf-8')
+    if isinstance(img_from_db, bytes):
+        return img_from_db.decode('utf-8')
+    if isinstance(img_from_db, memoryview):
+        return img_from_db.tobytes().decode('utf-8')
+    return img_from_db
