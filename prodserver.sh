0043a85d81dd8166e23ecbce75c7a09e02086af0
luismiaresse <luismiaresse03@gmail.com>
2024-07-22 10:52:06 +0200

    Change port to boot up unprivileged

--- prodserver.sh
+++ prodserver.sh
@@ -1,4 +1,6 @@
 
+mkdir -p logs
+
 # Vite build
 cd app
 npm run "build production"
@@ -11,4 +13,4 @@ sed -i 's/DEBUG = True/DEBUG = False/g' djangoconfig/settings.py
 python manage.py collectstatic --noinput
 
 # Start the server
-sudo python manage.py runmodwsgi --threads 12 --reload-on-changes --https-only --https-port=443 --http2 --user luismi --group luismi --ssl-certificate-file cert/2161476915.crt --ssl-certificate-key-file cert/private.key --ssl-ca-certificate-file cert/AAACertificateServices.crt --ssl-verify-client=/admin --server-name www.galeria.software
+python manage.py runmodwsgi --threads 12 --reload-on-changes --https-only --https-port=4430 --http2 --user luismi --group luismi --ssl-certificate-file cert/2161476915.crt --ssl-certificate-key-file cert/private.key --ssl-ca-certificate-file cert/AAACertificateServices.crt --ssl-verify-client=/admin --server-name www.galeria.software
