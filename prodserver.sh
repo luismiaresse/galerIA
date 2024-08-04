413738a40de5da3760a3c9a40d5bf4858592da43
luismiaresse <luismiaresse03@gmail.com>
2024-08-04 11:50:41 +0200

    Rebuild production images to reflect changes

--- prodserver.sh
+++ prodserver.sh
@@ -3,6 +3,7 @@
 sed -i 's/DEBUG = True/DEBUG = False/g' app/backend/djangoconfig/settings.py
 
 # Docker compose
+docker-compose -f docker-compose-prod.yml build
 docker-compose -f docker-compose-prod.yml up
 
 
